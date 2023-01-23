186f1d7af919b5a5029bb097c0045808915cac5f
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-01-23 14:02:46 +0100

    Fix: function edge case

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
+''' Simple helper function '''
 from typing import Tuple
-"""0. Simple helper function"""
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """a function named index_range that takes
-    two integer arguments page and page_size.
-
-    The function should return a tuple of size two containing a
-    start index and an end index corresponding to the range of indexes
-    to return in a list for those particular pagination parameters.
-    """
-    # start = (page - 1) * page_size
-    start = page * page_size - page_size
-    # end = page * page_size
-    end = start + page_size
-    return (start, end)
+def index_range(page: int, page_size: int) -> Tuple:
+    '''
+        function should return a tuple of size two
+        containing a start index and an end index
+        corresponding to the range of indexes to return in a
+        list for those particular pagination parameters
+    '''
+    start_index = page * page_size - page_size
+    stop_index = page * page_size
+    return (start_index, stop_index)
