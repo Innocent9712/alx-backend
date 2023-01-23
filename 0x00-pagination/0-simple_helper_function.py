45177ee6b73093619a83f600a68db5329f0cb2da
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-01-23 13:54:33 +0100

    Fix: function edge cae

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -13,5 +13,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
 
     start = (page - 1) * page_size
-    end = page * page_size
+    # end = page * page_size
+    end = start + page_size
     return (start, end)
